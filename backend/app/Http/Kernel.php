24a2782f30e9d115a1de33c2a52dc2cdef1777c0
mathcorreia <202409183211@alunos.unimetrocamp.edu.br>
2025-06-23 14:08:33 -0300

    cors

--- backend/app/Http/Kernel.php
+++ backend/app/Http/Kernel.php
@@ -1,4 +1,3 @@
-// backend/config/cors.php
 <?php
 
 return [
---
+++ backend/config/cors.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cross-Origin Resource Sharing (CORS) Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Aqui você pode configurar suas definições para o compartilhamento de
+    | recursos de origem cruzada (CORS).
+    |
+    */
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
+        'http://localhost:4200',      // Permite o acesso do seu frontend em desenvolvimento
+        'https://mathcorreia.github.io' // Permite o acesso do seu site publicado
+    ],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => false,
+
+];
